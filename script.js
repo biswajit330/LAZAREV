907bc99b73dcbc1f272fa73a29dc30d4ba5eda01
biswajit330 <bd533933@gmail.com>
2026-09-08 18:54:11 +0530

    Add Lazarev website

---
+++ script.js
@@ -0,0 +1,113 @@
+function navAnimation(){
+    var nav = document.querySelector("nav");
+
+nav.addEventListener("mouseenter", function () {
+  var tl = gsap.timeline();
+
+  tl.to("#nav-bottom", {
+    height: "21vh",
+  });
+
+  tl.set(".nav-part2 h5", {
+    display: "block",
+  });
+
+  tl.fromTo(
+    ".nav-part2 h5 span",
+    {
+      y: 25,
+    },
+    {
+      y: 0,
+      stagger: {
+        amount: 0.6,
+      },
+    },
+  );
+});
+
+nav.addEventListener("mouseleave", function () {
+  var tl = gsap.timeline();
+
+  tl.to(".nav-part2 h5 span", {
+    y: 25,
+    stagger: {
+      amount: 0.2,
+    },
+  });
+
+  tl.set(".nav-part2 h5", {
+    display: "none",
+  });
+
+  tl.to("#nav-bottom", {
+    height: 0,
+  });
+});
+}
+
+
+function page2ani(){
+  var rightElems=document.querySelectorAll(".right-elem")
+
+rightElems.forEach(function(elem){
+
+  elem.addEventListener("mouseenter",function(){
+    gsap.to(elem.childNodes[3],{
+      opacity:1,
+      scale:1
+    })
+  })
+  elem.addEventListener("mouseleave",function(){
+     gsap.to(elem.childNodes[3],{
+      opacity:0,
+      scale:0
+    })
+  })
+
+  elem.addEventListener("mousemove",function(dets){
+    gsap.to(elem.childNodes[3],{
+      x:dets.x-elem.getBoundingClientRect().x-130,
+      y:dets.y-elem.getBoundingClientRect().y-190
+    })
+  })
+
+
+})
+}
+
+
+
+
+    var sections = document.querySelectorAll(".sec-right")
+
+    sections.forEach(function (elem) {
+        elem.addEventListener("mouseenter", function () {
+            elem.childNodes[3].style.opacity = 1
+            elem.childNodes[3].play()
+        })
+        elem.addEventListener("mouseleave", function () {
+            elem.childNodes[3].style.opacity = 0
+            elem.childNodes[3].load()
+        })
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+navAnimation()
+page2ani()
+
